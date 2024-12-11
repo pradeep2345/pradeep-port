1cb791e53c9b551d90842cffc9ac908f394bde64
pradeep2345 <pradeep.nagavelu@gmail.com>
2024-12-11 16:09:04 +0530

    fix: gif added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import {HERO_CONTENT} from "../constants";
-import profilePic from "../assets/kevinRushProfile.png";
+import profilePic from "../assets/logo.gif";
 import {motion} from "framer-motion";
 
 const container = (delay) => ({
