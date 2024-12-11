bbd9fa911ac38a43a87440616cd9b3d594e20cbd
pradeep2345 <pradeep.nagavelu@gmail.com>
2024-12-11 16:12:14 +0530

    gif logo was added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import {HERO_CONTENT} from "../constants";
-import profilePic from "../assets/logo.gif";
+import profilePic from "../assets/logo.webp";
 import {motion} from "framer-motion";
 
 const container = (delay) => ({
